128695c8d1721ba93077e87056e3e819a88cf6b7
Marco Zakaria <marcozakaria@yahoo.com>
2024-08-01 12:09:06 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Marco on 2024-08-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
